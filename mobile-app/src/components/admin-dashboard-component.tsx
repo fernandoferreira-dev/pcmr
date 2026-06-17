d0875caf4475b24835df2f710c584e486fed1b03
jiminhoquaresma-hash <jiminhoquaresma@gmail.com>
2026-06-17 12:27:53 +0100

    feat: enhance admin dashboard layout and styles for server and sensor status

--- mobile-app/src/components/admin-dashboard-component.tsx
+++ mobile-app/src/components/admin-dashboard-component.tsx
@@ -1,7 +1,6 @@
 import DiagnosticButtonComponent from './diagnostic-button-component';
 import '../styles/styles.css';
 
-
 export default function AdminDashboardComponent() {
   return (
     <>
@@ -10,6 +9,16 @@ export default function AdminDashboardComponent() {
         <h1>Bem vindo, Admin</h1>
         <div className="mainbox">  
           <DiagnosticButtonComponent/>
+          <div className="main-pages-states">
+            <div className="main-page-states-box">
+              <h2>Estado do servidor: </h2>
+              <div className="OKstate"></div>
+            </div>
+            <div className="main-page-states-box">
+              <h2>Estado do sensor nó: </h2>
+              <div className="OKstate"></div>
+            </div>
+          </div>
         </div>
       </div>
     </>
