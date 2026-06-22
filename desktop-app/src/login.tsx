46facbba592f951a0bae38fb5b66788f5302ad67
Diogo <2025141335@estudantes.ips.pt>
2026-06-22 10:20:47 +0100

    Teste

--- desktop-app/src/login.tsx
+++ desktop-app/src/login.tsx
@@ -25,16 +25,16 @@ export default function App() {
           <div>
             <h2 className="text-3xl font-semibold text-gray-700 mb-7 text-center">MedyCist</h2>
 
-            <label className="block text-sm text-gray-600 mb-2">Utilizador</label>
+            <label className="block text-1xl text-gray-600 mb-2">Utilizador</label>
             <input
-              className="w-full mb-4 px-4 py-2 border-2 border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-green-300"
+              className="w-full mb-4 px-5 py-2 border-2 border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-green-300"
               placeholder="Introduza o seu utilizador..."
             />
 
-            <label className="block text-sm text-gray-600 mb-2">Palavra-passe</label>
+            <label className="block text-1xl text-gray-600 mb-2">Palavra-passe</label>
             <input
               type="Palavra-passe"
-              className="w-full mb-2 px-4 py-2 border-2 border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-green-300"
+              className="w-full mb-2 px-5 py-2 border-2 border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-green-300"
               placeholder="Introduza a sua palavra-passe...."
             />
 
