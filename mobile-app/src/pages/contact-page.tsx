3d6f49018c4458cb7ffe0021f55e589052ced264
jiminhoquaresma-hash <jiminhoquaresma@gmail.com>
2026-06-24 12:20:25 +0100

    feat: add MessageComponent and styles for message notifications in contact page

--- mobile-app/src/pages/contact-page.tsx
+++ mobile-app/src/pages/contact-page.tsx
@@ -1,8 +1,10 @@
+import MessageComponent from "../components/comms-page/message-component"
 export default function ContactPage() {
     return (
         <>
             <h1>Contactos</h1>
             <p>Contactos placeholder</p>
+            <MessageComponent/>
         </>
     )
 }
