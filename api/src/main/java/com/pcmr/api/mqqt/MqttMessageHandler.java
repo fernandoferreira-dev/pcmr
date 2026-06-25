7a282c6a334232f2ab62627c80b2d35a64f71d44
fernandoferreira-dev <fernandofilipeferreira.06@gmail.com>
2026-06-25 21:27:57 +0100

    feat: mqqt connectivity and logging

---
+++ api/src/main/java/com/pcmr/api/mqqt/MqttMessageHandler.java
@@ -0,0 +1,21 @@
+package com.pcmr.api.configuration;
+
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MqttMessageHandler {
+
+    @ServiceActivator(inputChannel = "mqttInputChannel")
+    public void handleMessage(Message<?> message) {
+        String payload = message.getPayload().toString();
+        String topic = message.getHeaders()
+                              .get("mqtt_receivedTopic", String.class);
+
+        System.out.println("Tópico: " + topic);
+        System.out.println("Payload: " + payload);
+
+        // https://www.youtube.com/watch?v=KK5uGdYGo80
+    }
+}
