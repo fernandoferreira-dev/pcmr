bfd11e075b19c7e88dae0d233a50130e16e11cf4
jiminhoquaresma-hash <--get>
2026-06-27 21:06:01 +0100

    feat: implement SettingsButtonComponent with modal overlay; add styles for header and settings button. Added a mock up settings pop up

--- mobile-app/src/components/misc/settings-button-component.tsx
+++ mobile-app/src/components/misc/settings-button-component.tsx
@@ -1,13 +1,26 @@
+import { useState } from "react";
 import SettingsIcon from "../../assets/settings-icon.png"
+import "../../styles/misc/settings-btn-styles.css"
 
 export default function SettingsButtonComponent() {
+    const [isOpen, setIsOpen] = useState(false);
+    
     return(
         <>
-            <button>
-                <div className="settings-container">
-                    <img className="settings-icon" src={SettingsIcon} alt="Settings"></img>
-                </div>
-            </button>
+            <div className="settings-container">
+                <button onClick={() => setIsOpen(true)} className="settings-container-button">
+                        <img className="settings-icon" src={SettingsIcon} alt="Settings"></img>
+                </button>
+
+                {isOpen && (
+                  <div className="overlay">
+                    <div className="modal">
+                      <p>popup de teste</p>
+                      <button onClick={() => setIsOpen(false)}>Close</button>
+                    </div>
+                  </div>
+                )}
+            </div>
         </>
     )
 }
--- mobile-app/src/pages/main-page.tsx
+++ mobile-app/src/pages/main-page.tsx
@@ -5,7 +5,8 @@ import ContactPage from './contact-page'
 import StatusPage from './status-page'
 import InfoPage from './info-page'
 import ClientInfoPage from './client-info-page'
-
+import SettingsButtonComponent from "../components/misc/settings-button-component"
+import "../styles/misc/header-styles.css" 
 export default function MainPage() {
     const [view, setView] = useState<'home' | 'contact' | 'status' | 'doctor' | 'records'>('home')
 
@@ -26,6 +27,9 @@ export default function MainPage() {
 
     return (
         <>
+            <div className="header-container">
+                <SettingsButtonComponent/>
+            </div>
             {renderView()}
             <NavBarComponent onNavigate={setView} />
         </>
