9c133c646afad19fb95756eb357e432b550a3e48
jiminhoquaresma <--get>
2026-06-30 16:08:58 +0100

    feat: added a combo box for language preference, added a button for log out and added a toggleable darkmode theme

--- mobile-app/src/components/misc/settings-button-component.tsx
+++ mobile-app/src/components/misc/settings-button-component.tsx
@@ -1,10 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SettingsIcon from "../../assets/settings-icon.png"
 import "../../styles/misc/settings-btn-styles.css"
+import "../../styles/login-page/login-page-styles.css"
 
 export default function SettingsButtonComponent() {
     const [isOpen, setIsOpen] = useState(false);
     const [darkEnabled, setIsEnabled] = useState(false);
+    const [selectedOption, setSelectedOption] = useState("opcao1");
+
+    useEffect(() => {
+      document.body.classList.toggle("dark-theme", darkEnabled);
+
+      return () => {
+        document.body.classList.remove("dark-theme");
+      };
+    }, [darkEnabled]);
     
     return(
         <>
@@ -18,16 +28,27 @@ export default function SettingsButtonComponent() {
                     <div className="modal">
                       <p>Definições</p>
                       <div className="settings">
-                        <label className="switch">
-                          <input
-                            type="checkbox"
-                            checked={darkEnabled}
-                            onChange={() => setIsEnabled(!darkEnabled)}
-                            />
-                          <span className="slider"></span>
-                        </label>
-                        <p>Modo Escuro</p>
-                      </div>
+                        <div className="option-container">
+                          <label className="switch">
+                            <input
+                              type="checkbox"
+                              checked={darkEnabled}
+                              onChange={() => setIsEnabled(!darkEnabled)}
+                              />
+                            <span className="slider"></span>
+                          </label>
+                          <p>Modo Escuro</p>
+                        </div>
+                        <div className="option-container">
+                          <select value={selectedOption} onChange={(e) => setSelectedOption(e.target.value)}>
+                            <option value="opcao1">Português</option>
+                            <option value="opcao2">Inglês</option>
+                            <option value="opcao3">Espanhol</option>
+                          </select>
+                          <p>Preferência de Linguagem</p>
+                        </div>
+                    </div>
+                      <button className="log-out-btn">Terminar Sessão</button>
                       <button onClick={() => setIsOpen(false)}>Close</button>
                     </div>
                   </div>
