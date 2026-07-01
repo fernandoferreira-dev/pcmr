10961e8e5bafc13eade0094e310e198bce07f2e3
jiminhoquaresma <--get>
2026-07-01 14:08:42 +0100

    testcommit

--- mobile-app/src/pages/main-page.tsx
+++ mobile-app/src/pages/main-page.tsx
@@ -1,53 +1,55 @@
-import { useState } from 'react'
-import AdminDashboardComponent from '../components/dashboard/admin-dashboard-component'
-import NavBarComponent from '../components/misc/nav-bar-component'
-import ContactPage from './contact-page'
-import StatusPage from './status-page'
-import InfoPage from './info-page'
-import ClientInfoPage from './client-info-page'
-import SettingsButtonComponent from "../components/misc/settings-button-component"
-import SendEmailButtonComponent from "../components/misc/send-email-btn"
-import "../styles/misc/header-styles.css" 
+import { useState } from "react";
+import AdminDashboardComponent from "../components/dashboard/admin-dashboard-component";
+import NavBarComponent from "../components/misc/nav-bar-component";
+import ContactPage from "./contact-page";
+import StatusPage from "./status-page";
+import InfoPage from "./info-page";
+import ClientInfoPage from "./client-info-page";
+import SettingsButtonComponent from "../components/misc/settings-button-component";
+import SendEmailButtonComponent from "../components/misc/send-email-btn";
+import "../styles/misc/header-styles.css";
 
 export default function MainPage() {
-    const [view, setView] = useState<'home' | 'comms' | 'status' | 'doctor' | 'records'>('home')
+  const [view, setView] = useState<
+    "home" | "comms" | "status" | "doctor" | "records"
+  >("home");
 
-    const pageTitles = {
-        home:       'Medycist - Dashboard',
-        comms:      'Medycist - Comunicação',
-        status:     'Medycist - Dados de Equipamentos',
-        doctor:     'Medycist - Dados Pessoais',
-        records:    'Medycist - Dados Diagnósticos',
-    } as const
+  const pageTitles = {
+    home: "Medycist test - Dashboard",
+    comms: "Medycist - Comunicação",
+    status: "Medycist - Dados de Equipamentos",
+    doctor: "Medycist - Dados Pessoais",
+    records: "Medycist - Dados Diagnósticos",
+  } as const;
 
-    const renderView = () => {
-        switch (view) {
-            case 'comms':
-                return <ContactPage/>
-            case 'status':
-                return <StatusPage/>
-            case 'doctor':
-                return <InfoPage/>
-            case 'records':
-                return <ClientInfoPage/>
-            default:
-                return <AdminDashboardComponent/>
-        }
+  const renderView = () => {
+    switch (view) {
+      case "comms":
+        return <ContactPage />;
+      case "status":
+        return <StatusPage />;
+      case "doctor":
+        return <InfoPage />;
+      case "records":
+        return <ClientInfoPage />;
+      default:
+        return <AdminDashboardComponent />;
     }
+  };
 
-    return (
-        <>
-            <div className="header-container">
-                <div className="header-row">
-                    <h1 className="header-page-title">{pageTitles[view]}</h1>
-                    <div className="buttons-row">
-                        <SettingsButtonComponent/>
-                        <SendEmailButtonComponent/>s
-                    </div>
-                </div>
-            </div>
-            {renderView()}
-            <NavBarComponent onNavigate={setView} />
-        </>
-    )
+  return (
+    <>
+      <div className="header-container">
+        <div className="header-row">
+          <h1 className="header-page-title">{pageTitles[view]}</h1>
+          <div className="buttons-row">
+            <SettingsButtonComponent />
+            <SendEmailButtonComponent />
+          </div>
+        </div>
+      </div>
+      {renderView()}
+      <NavBarComponent onNavigate={setView} />
+    </>
+  );
 }
