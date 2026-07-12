1fb0eb32acde9fa71778708adc0c629bc3c8b01e
fernandoferreira-dev <fernandofilipeferreira.06@gmail.com>
2026-07-12 23:43:26 +0100

    slop drop

--- mobile-app/src/pages/contact-page.tsx
+++ mobile-app/src/pages/contact-page.tsx
@@ -1,5 +1,4 @@
 import MessageComponent from "../components/comms-page/message-component"
-import MessageSearchBarComponent from "../components/comms-page/message-search-bar-component"
 import "../styles/messages-page-styles/message-box-styles.css"
 
 export default function ContactPage() {
@@ -7,7 +6,6 @@ export default function ContactPage() {
         <>
             <div className="message-wrapper">
                 <h1>Mensagens recentes</h1>
-                <MessageSearchBarComponent />
                 <MessageComponent />
             </div>
         </>
---
+++ paciente-app/src/pages/login-page.tsx
@@ -0,0 +1,13 @@
+import "../assets/css/index.css"
+
+function LoginPage() {
+  return (
+      <>
+        <div className="bg-bacl">
+
+        </div>
+      </>
+  );
+}
+
+export default LoginPage
