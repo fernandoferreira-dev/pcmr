2670de8cffdcaa8c360645cf65d782739dbc461a
fernandoferreira-dev <fernandofilipeferreira.06@gmail.com>
2026-07-13 01:26:47 +0100

    ooha booga

--- paciente-app/src/pages/login-page.tsx
+++ paciente-app/src/pages/login-page.tsx
@@ -1,10 +1,11 @@
-import "../assets/css/index.css"
+import "../assets/styles/index.css"
+import { Button } from "../components"
 
 function LoginPage() {
   return (
       <>
         <div className="bg-bacl">
-
+            <Button>Entrar</Button>
         </div>
       </>
   );
