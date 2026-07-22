a413e1214eee1ed4cd3d517e7de1b8dbffc8af9f
fernandoferreira-dev <fernandofilipeferreira.06@gmail.com>
2026-07-22 15:27:19 +0100

    fix: missing API files after merge UtilizadorPacienteAcessorepository.java

--- api/src/main/java/com/pcmr/api/repository/UtilizadorPacienteAcessoRepository.java
+++ api/src/main/java/com/pcmr/api/repository/UtilizadorPacienteAcessoRepository.java
@@ -4,6 +4,9 @@ import com.pcmr.api.model.UtilizadorPacienteAcesso;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UtilizadorPacienteAcessoRepository extends JpaRepository<UtilizadorPacienteAcesso, Long> {
+    Optional<UtilizadorPacienteAcesso> findByTokenAcesso(String tokenAcesso);
 }
